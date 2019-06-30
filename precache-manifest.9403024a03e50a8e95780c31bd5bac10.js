50293f0ea9f4f8882e77d5698983d850d0e03a0e
Yogita Verma <veryogita@gmail.com>
2019-06-30 16:38:35 +0530

    Updates

---
+++ precache-manifest.9403024a03e50a8e95780c31bd5bac10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cfcbba580ea6374ff739e0b5d635f5d",
+    "url": "/SnapScout/index.html"
+  },
+  {
+    "revision": "a771819ccdedc96a78a5",
+    "url": "/SnapScout/static/css/main.935d6ac5.chunk.css"
+  },
+  {
+    "revision": "ec5128440ca97b9ebaa5",
+    "url": "/SnapScout/static/js/2.0e3004c5.chunk.js"
+  },
+  {
+    "revision": "a771819ccdedc96a78a5",
+    "url": "/SnapScout/static/js/main.613546b5.chunk.js"
+  },
+  {
+    "revision": "ac29b5fa8e8f830c4d20",
+    "url": "/SnapScout/static/js/runtime~main.9f8235c8.js"
+  }
+]);
